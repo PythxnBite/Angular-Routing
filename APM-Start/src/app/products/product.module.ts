671b769a2eaf6d29eb59af3f7b468c08f10187b1
Sushanth Reddy Udumula <sushanthreddyu@gmail.com>
2020-05-22 18:06:53 +0530

    Router Guards complete

--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core'
 import { RouterModule } from '@angular/router'
 
 import { ProductListComponent } from './product-list.component'
+
 import { ProductDetailComponent } from './product-detail.component'
-import { ProductEditComponent } from './product-edit/product-edit.component'
+
 import { ProductResolver } from './product-resolver.service'
 
-import { SharedModule } from '../shared/shared.module'
+import { ProductEditComponent } from './product-edit/product-edit.component'
 import { ProductEditInfoComponent } from './product-edit/product-edit-info.component'
 import { ProductEditTagsComponent } from './product-edit/product-edit-tags.component'
+
+import { SharedModule } from '../shared/shared.module'
+
+import { AuthGuard } from '../user/auth.guard'
+import { ProductEditGuard } from './product-edit/product-edit.guard'
 
 @NgModule({
 	imports: [
@@ -16,7 +22,7 @@ import { ProductEditTagsComponent } from './product-edit/product-edit-tags.compo
 		RouterModule.forChild([
 			{
 				path: 'products',
-
+				canActivate: [AuthGuard],
 				children: [
 					{
 						path: '',
@@ -32,6 +38,7 @@ import { ProductEditTagsComponent } from './product-edit/product-edit-tags.compo
 					{
 						path: ':id/edit',
 						component: ProductEditComponent,
+						canDeactivate: [ProductEditGuard],
 						resolve: {
 							resolvedData: ProductResolver,
 						},
